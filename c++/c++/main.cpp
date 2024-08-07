98691b9cccfb4cb5f910324ef2ea917d4ef33172
Aditya Kumar singh <130950599+adiadamomdad1147@users.noreply.github.com>
2024-08-07 12:14:45 +0530

    uniass

---
+++ c++/c++/main.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+void f1()
+{
+    int a,b;
+    cout<<"Enter two numbers";
+    cin>>a>>b;
+    
+}
+int main()
+{
+    f1();
+    int a,b;
+    if(a>b)
+        cout<<"a is greater than b";
+    else
+        cout<<"b is greater";
+            <#statements#>
+        }
+}
